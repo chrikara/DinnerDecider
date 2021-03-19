e7f31e6faa9ed09ca911b8743dc9b0f3ad004932
Chris <xristoskarasoulas@gmail.com>
2021-03-19 13:26:43 +0200

    Twra evala z

--- app/src/main/java/com/example/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/example/dinnerdecider/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var y = 4
+        var z = "5"
     }
 }
